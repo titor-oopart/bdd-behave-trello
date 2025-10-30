bc446160ee6855668b70e3e4514e1484646be93a
titor oopart <johnpiterzon@gmail.com>
2025-10-30 12:21:55 -0400

    api consumer implemented.

---
+++ api/api_consumer.py
@@ -0,0 +1,59 @@
+from requests import Request, Session
+
+
+class ApiConsumer:
+    __instance = None
+
+    def __init__(self, base_url, api_key, token):
+        self.session = Session()
+        self.base_url = base_url
+        self.api_key = api_key
+        self.token = token
+        self.session.headers.update({"Accept": "application/json"})
+
+    @classmethod
+    def get_instance(cls, context):
+        if cls.__instance == None:
+            cls.__instance = cls(
+                base_url=context.BASE_URL,
+                api_key=context.API_KEY,
+                token=context.API_TOKEN,
+            )
+        return cls.__instance
+
+    def api_request(self, endpoint, method, params=None, data=None):
+        if params is None:
+            params = {}
+        params["key"] = self.api_key
+        params["token"] = self.token
+        print(method, params, data)
+        req = Request(
+            url=f"{self.base_url.rstrip('/')}/{endpoint.lstrip('/')}",
+            method=method,
+            params=params,
+            data=data,
+        )
+        response = self.session.send(req.prepare())
+        return response
+
+    def get(self, endpoint):
+        response = self.api_request(
+            endpoint,
+            "GET",
+        )
+        return response.text
+
+    def post(self, endpoint, params, body):
+        response = self.api_request(endpoint, "POST", params, body)
+        return response.text
+
+    def put(self, endpoint, params):
+        response = self.api_request(endpoint, params)
+        return response.text
+
+    def delete(self, endpoint):
+        response = self.api_request(
+            endpoint,
+            "DELETE",
+        )
+        return response.text

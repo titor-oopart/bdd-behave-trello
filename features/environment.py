8b842076244c4468aa745a0e034924b8530f3ad6
titor oopart <johnpiterzon@gmail.com>
2025-10-31 19:54:06 -0400

    Logger implemented

--- features/environment.py
+++ features/environment.py
@@ -2,11 +2,21 @@ import os
 from dotenv import load_dotenv
 from driver import driver
 from features.steps.navigation.navigation_page import NavigationPage
+import logging
 
 load_dotenv()
 
 
 def before_all(context):
+    logger_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(
+        filename="bdd-behave-trello.log",
+        level=logging.INFO,
+        datefmt="%a, %d %b %Y %H:%M:%S",
+        format=logger_format,
+    )
+    logger = logging.getLogger(__name__)
+
     context.driver = driver.get_driver()
     context.driver.fullscreen_window()
     context.USER_NAME = os.getenv("USER_NAME")
@@ -17,6 +27,10 @@ def before_all(context):
     context.API_TOKEN = os.getenv("API_TOKEN")
     context.navigation = NavigationPage(context)
 
+    logger.info("=== Test session started ===")
+    context.logger = logger
+
 
 def after_all(context):
     context.driver.close()
+    context.logger.info("=== Test session finished ===")
